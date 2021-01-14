b05463a19a3742759f56ece476952711bbd100e4
Raja Kalavala <rajakalavala55@gmail.com>
2021-01-14 14:27:05 +0530

    Add Sidebar component and add Compose button UI in it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
-      <h2>Lets Build Gmail</h2>
+      <Sidebar></Sidebar>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+import AddIcon from "@material-ui/icons/Add";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <Button
+        className="sidebar__compose"
+        startIcon={<AddIcon fontSize="large" />}
+      >
+        Compose
+      </Button>
+    </div>
+  );
+}
+
+export default Sidebar;
