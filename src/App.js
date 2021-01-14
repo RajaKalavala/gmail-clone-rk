344b0881bfbd610d8029b7839a9ad7b16ad6688f
Raja Kalavala <rajakalavala55@gmail.com>
2021-01-14 16:17:41 +0530

    Add React Router
    Added email list icon as well

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar/Sidebar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Mail from "./Mail/Mail";
+import EmailList from "./EmailList/EmailList";
 
 function App() {
   return (
-    <div className="app">
-      <Header></Header>
-      <Sidebar></Sidebar>
-    </div>
+    <Router>
+      <div className="app">
+        <Header></Header>
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/EmailList/EmailList.js
@@ -0,0 +1,52 @@
+import { Checkbox, IconButton } from "@material-ui/core";
+import {
+  ArrowDropDown,
+  ChevronLeft,
+  ChevronRight,
+  KeyboardHide,
+  MoreVert,
+  Redo,
+  Settings,
+} from "@material-ui/icons";
+import React from "react";
+import "./EmailList.css";
+
+function EmailList() {
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDown />
+          </IconButton>
+          <IconButton>
+            <Redo />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeft />
+          </IconButton>
+          <IconButton>
+            <ChevronRight />
+          </IconButton>
+          <IconButton>
+            <KeyboardHide />
+          </IconButton>
+          <IconButton>
+            <Settings />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="emailList__section"></div>
+    </div>
+  );
+}
+
+export default EmailList;
