c056c90ba6e0aa89492c1beb4f1fc2138854a94d
Raja Kalavala <rajakalavala55@gmail.com>
2021-01-14 15:22:12 +0530

    Complete Sidebar UI
    Also update folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
-import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
   return (
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,59 @@
+import { Button, IconButton } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SidebarOption from "../SidebarOption/SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+import {
+  AccessTime,
+  Duo,
+  ExpandMore,
+  LabelImportant,
+  NearMe,
+  Note,
+  Person,
+  Phone,
+} from "@material-ui/icons";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <Button
+        className="sidebar__compose"
+        startIcon={<AddIcon fontSize="large" />}
+      >
+        Compose
+      </Button>
+
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={30}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={7} />
+      <SidebarOption Icon={AccessTime} title="Snoozed" number={5} />
+      <SidebarOption Icon={LabelImportant} title="Important" number={62} />
+      <SidebarOption Icon={NearMe} title="Sent" number={6} />
+      <SidebarOption Icon={Note} title="Drafts" number={12} />
+      <SidebarOption Icon={ExpandMore} title="More" number={1} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <Person />
+          </IconButton>
+          <IconButton>
+            <Duo />
+          </IconButton>
+          <IconButton>
+            <Phone />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
